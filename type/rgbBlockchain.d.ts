1bb7548d72960368dabd2be6d5baadaca23708df
saurav singh <saurav@coindelta.io>
2024-04-08 17:28:46 +0530

    random change

--- type/rgbBlockchain.d.ts
+++ type/rgbBlockchain.d.ts
@@ -1,11 +1,20 @@
-import { BurnParamsRgb, CreateTokenParamsRgb, TransferBaseParamsRgb, createFuntionReturnInterface, rgbContructorParams, TransferFunReturnInterface } from "./interfaces/BlockchainSpecificParamsInterface";
+import {
+  BurnParamsRgb,
+  CreateTokenParamsRgb,
+  TransferBaseParamsRgb,
+  createFuntionReturnInterface,
+  rgbContructorParams,
+  TransferFunReturnInterface,
+} from "./interfaces/BlockchainSpecificParamsInterface";
 import { RgbBlockchainInterface } from "./interfaces/BlockchainSpecificInterface";
 declare class Rgb implements RgbBlockchainInterface {
-    private rgbApi;
-    private walletAddress;
-    constructor(config: rgbContructorParams);
-    createToken(params: CreateTokenParamsRgb): Promise<createFuntionReturnInterface>;
-    burn(params: BurnParamsRgb): Promise<void>;
-    transfer(params: TransferBaseParamsRgb): Promise<TransferFunReturnInterface>;
+  private rgbApi;
+  private walletAddress;
+  constructor(config: rgbContructorParams);
+  createToken(
+    params: CreateTokenParamsRgb
+  ): Promise<createFuntionReturnInterface>;
+  burn(params: BurnParamsRgb): Promise<void>;
+  transfer(params: TransferBaseParamsRgb): Promise<TransferFunReturnInterface>;
 }
 export { Rgb };
